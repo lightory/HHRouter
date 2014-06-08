f01db7ec1cf71948772327b3ebb610e33750e1f1
张忻正 <zhangxinzheng@huohua.in>
2014-06-08 16:35:27 +0800

    modify input framework

--- HHRouter/HHRouter.h
+++ HHRouter/HHRouter.h
@@ -20,7 +20,7 @@
 //  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 //  THE SOFTWARE.
 
-#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
 
 ///---------------
 /// @name HHRouter
