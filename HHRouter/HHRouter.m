0cdf01b864bb1ae9a39cd6cca2763f1bba5950a9
Elethom Hunter <elethomhunter@gmail.com>
2014-12-23 10:07:43 +0800

    Fix route components parsing

--- HHRouter/HHRouter.m
+++ HHRouter/HHRouter.m
@@ -167,7 +167,7 @@
 - (NSArray*)pathComponentsFromRoute:(NSString*)route
 {
     NSMutableArray *pathComponents = [NSMutableArray array];
-    for (NSString *pathComponent in [[NSURL URLWithString:route] pathComponents]) {
+    for (NSString *pathComponent in route.pathComponents) {
         if ([pathComponent isEqualToString:@"/"]) continue;
         if ([[pathComponent substringToIndex:1] isEqualToString:@"?"]) break;
         [pathComponents addObject:pathComponent];
@@ -246,3 +246,4 @@ static char kAssociatedParamsObjectKey;
 }
 
 @end
+
